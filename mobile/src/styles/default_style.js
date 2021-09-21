4c4bc7363f33f7a8c0b1281a984de6cc265f04e4
Jederilson Sousa Luz <Jederilson@gmail.com>
2021-09-21 12:23:50 -0300

    Update default_style.js
    Novas cores para cada raridade de item.

--- mobile/src/styles/default_style.js
+++ mobile/src/styles/default_style.js
@@ -1,11 +1,19 @@
-import { StyleSheet } from 'react-native';
-
-colors = {
+const colors = {
     primary_color: '#F0EBE4',
     secondary_color: '#E7DED1',
     ternary_color: '#CFC2AF',
     strong_color: '#85613E',
     weak_color: '#F5F2EB',
-}
+    rarity_1_header_color: '#72778B',
+    rarity_2_header_color: '#299072',
+    rarity_3_header_color: '#5081CC',
+    rarity_4_header_color: '#A356E2',
+    rarity_5_header_color: '#BC6A32',
+    rarity_1_color: '#7E8693',
+    rarity_2_color: '#5D926F',
+    rarity_3_color: '#53A1B4',
+    rarity_4_color: '#BA87C9',
+    rarity_5_color: '#7E8693',
+};
 
 export default colors;
