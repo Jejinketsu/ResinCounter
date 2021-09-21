fdda3ab2910d7890ee5e9937816258f9fbbfa6d4
Jederilson Sousa Luz <Jederilson@gmail.com>
2021-09-21 10:52:46 -0300

    Create dataMook.js
    Arquivo com dados estáticos apenas para teste

---
+++ mobile/src/mook/data/dataMook.js
@@ -0,0 +1,217 @@
+export const domain_rewards = {
+    Monday: [
+        {
+            key: '1',
+            name: "Tile of Decarabian's Tower",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/d/d7/Item_Tile_of_Decarabian%27s_Tower.png/revision/latest/scale-to-width-down/256?cb=20210109220205',
+        },
+        {
+            key: '2',
+            name: "Debris of Decarabian's City",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/9/96/Item_Debris_of_Decarabian%27s_City.png/revision/latest/scale-to-width-down/256?cb=20210109220117',
+        },
+        {
+            key: '3',
+            name: "Fragment of Decarabian's Epic",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/8/8a/Item_Fragment_of_Decarabian%27s_Epic.png/revision/latest/scale-to-width-down/256?cb=20210109220101',
+        },
+        {
+            key: '4',
+            name: "Scattered Piece of Decarabian's Dream",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/4/4c/Item_Scattered_Piece_of_Decarabian%27s_Dream.png/revision/latest/scale-to-width-down/256?cb=20210109220111',
+        },
+    ],
+    Tuesday: [
+        {
+            key: '1',
+            name: "Tile of Decarabian's Tower",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/d/d7/Item_Tile_of_Decarabian%27s_Tower.png/revision/latest/scale-to-width-down/256?cb=20210109220205',
+        },
+        {
+            key: '2',
+            name: "Debris of Decarabian's City",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/9/96/Item_Debris_of_Decarabian%27s_City.png/revision/latest/scale-to-width-down/256?cb=20210109220117',
+        },
+        {
+            key: '3',
+            name: "Fragment of Decarabian's Epic",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/8/8a/Item_Fragment_of_Decarabian%27s_Epic.png/revision/latest/scale-to-width-down/256?cb=20210109220101',
+        },
+        {
+            key: '4',
+            name: "Scattered Piece of Decarabian's Dream",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/4/4c/Item_Scattered_Piece_of_Decarabian%27s_Dream.png/revision/latest/scale-to-width-down/256?cb=20210109220111',
+        },
+    ],
+    Wednesday: [
+        {
+            key: '1',
+            name: "Tile of Decarabian's Tower",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/d/d7/Item_Tile_of_Decarabian%27s_Tower.png/revision/latest/scale-to-width-down/256?cb=20210109220205',
+        },
+        {
+            key: '2',
+            name: "Debris of Decarabian's City",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/9/96/Item_Debris_of_Decarabian%27s_City.png/revision/latest/scale-to-width-down/256?cb=20210109220117',
+        },
+        {
+            key: '3',
+            name: "Fragment of Decarabian's Epic",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/8/8a/Item_Fragment_of_Decarabian%27s_Epic.png/revision/latest/scale-to-width-down/256?cb=20210109220101',
+        },
+        {
+            key: '4',
+            name: "Scattered Piece of Decarabian's Dream",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/4/4c/Item_Scattered_Piece_of_Decarabian%27s_Dream.png/revision/latest/scale-to-width-down/256?cb=20210109220111',
+        },
+    ],
+    Thursday: [
+        {
+            key: '1',
+            name: "Tile of Decarabian's Tower",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/d/d7/Item_Tile_of_Decarabian%27s_Tower.png/revision/latest/scale-to-width-down/256?cb=20210109220205',
+        },
+        {
+            key: '2',
+            name: "Debris of Decarabian's City",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/9/96/Item_Debris_of_Decarabian%27s_City.png/revision/latest/scale-to-width-down/256?cb=20210109220117',
+        },
+        {
+            key: '3',
+            name: "Fragment of Decarabian's Epic",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/8/8a/Item_Fragment_of_Decarabian%27s_Epic.png/revision/latest/scale-to-width-down/256?cb=20210109220101',
+        },
+        {
+            key: '4',
+            name: "Scattered Piece of Decarabian's Dream",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/4/4c/Item_Scattered_Piece_of_Decarabian%27s_Dream.png/revision/latest/scale-to-width-down/256?cb=20210109220111',
+        },
+    ],
+    Friday: [
+        {
+            key: '1',
+            name: "Tile of Decarabian's Tower",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/d/d7/Item_Tile_of_Decarabian%27s_Tower.png/revision/latest/scale-to-width-down/256?cb=20210109220205',
+        },
+        {
+            key: '2',
+            name: "Debris of Decarabian's City",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/9/96/Item_Debris_of_Decarabian%27s_City.png/revision/latest/scale-to-width-down/256?cb=20210109220117',
+        },
+        {
+            key: '3',
+            name: "Fragment of Decarabian's Epic",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/8/8a/Item_Fragment_of_Decarabian%27s_Epic.png/revision/latest/scale-to-width-down/256?cb=20210109220101',
+        },
+        {
+            key: '4',
+            name: "Scattered Piece of Decarabian's Dream",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/4/4c/Item_Scattered_Piece_of_Decarabian%27s_Dream.png/revision/latest/scale-to-width-down/256?cb=20210109220111',
+        },
+    ],
+    Saturday: [
+        {
+            key: '1',
+            name: "Tile of Decarabian's Tower",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/d/d7/Item_Tile_of_Decarabian%27s_Tower.png/revision/latest/scale-to-width-down/256?cb=20210109220205',
+        },
+        {
+            key: '2',
+            name: "Debris of Decarabian's City",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/9/96/Item_Debris_of_Decarabian%27s_City.png/revision/latest/scale-to-width-down/256?cb=20210109220117',
+        },
+        {
+            key: '3',
+            name: "Fragment of Decarabian's Epic",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/8/8a/Item_Fragment_of_Decarabian%27s_Epic.png/revision/latest/scale-to-width-down/256?cb=20210109220101',
+        },
+        {
+            key: '4',
+            name: "Scattered Piece of Decarabian's Dream",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/4/4c/Item_Scattered_Piece_of_Decarabian%27s_Dream.png/revision/latest/scale-to-width-down/256?cb=20210109220111',
+        },
+    ],
+    Sunday: [
+        {
+            key: '1',
+            name: 'Fetters of the Dandelion Gladiator',
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/0/05/Item_Fetters_of_the_Dandelion_Gladiator.png/revision/latest/scale-to-width-down/256?cb=20210109220002',
+        },
+        {
+            key: '2',
+            name: "Debris of Decarabian's City",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/9/96/Item_Debris_of_Decarabian%27s_City.png/revision/latest/scale-to-width-down/256?cb=20210109220117',
+        },
+        {
+            key: '3',
+            name: "Fragment of Decarabian's Epic",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/8/8a/Item_Fragment_of_Decarabian%27s_Epic.png/revision/latest/scale-to-width-down/256?cb=20210109220101',
+        },
+        {
+            key: '4',
+            name: "Scattered Piece of Decarabian's Dream",
+            uri:
+                'https://static.wikia.nocookie.net/gensin-impact/images/4/4c/Item_Scattered_Piece_of_Decarabian%27s_Dream.png/revision/latest/scale-to-width-down/256?cb=20210109220111',
+        },
+    ],
+};
+
+export const allDomains = [
+    {
+        name: 'Cecilian Garden',
+        type: 'Weapon Ascension Material',
+        rewards: domain_rewards,
+    },
+    {
+        name: 'Hidden Palace of Lianshan Formula',
+        type: 'Weapon Ascension Material',
+        rewards: domain_rewards,
+    },
+    {
+        name: 'Taishan Mansion',
+        type: 'Talent Level-Up Material',
+        rewards: domain_rewards,
+    },
+    {
+        name: 'Forsaken Rift',
+        type: 'Talent Level-Up Material',
+        rewards: domain_rewards,
+    },
+];
+
+export const days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+];
